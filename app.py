d6d2306bc64ab3360d005467bd47d07db13b56e6
Amanpreet Singh <amanpreetsingh459@gmail.com>
2021-07-19 13:46:49 +0530

    Update the files with done exercises

--- app.py
+++ app.py
@@ -1,39 +1,38 @@
-from flask import Flask
-from flask import json
+from flask import Flask, json
 import logging
-
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/status')
-def healthcheck():
+
+@app.route("/status")
+def get_status():
     response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Status request successfull')
+                        response=json.dumps({"result": "OK - healthy"}),
+                        status=200,
+                        mimetype='application/json'
+        )
+    app.logger.info("Status request successful")
     return response
 
-@app.route('/metrics')
-def metrics():
-    response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
-    )
 
-    app.logger.info('Metrics request successfull')
+@app.route("/metrics")
+def get_metrics():
+    response = app.response_class(
+                        response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+                        status=200,
+                        mimetype='application/json'
+        )
+    app.logger.info("Metrics request successful")
     return response
+
 
 @app.route("/")
 def hello():
-    app.logger.info('Main request successfull')
-
-    return "Hello World!"
+    app.logger.info("Main request was successful")
+    return "Hello World! Starting Microservices Project..."
 
 if __name__ == "__main__":
-    ## stream logs to a file
+    ## stream logs to app.log file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
     
     app.run(host='0.0.0.0')
